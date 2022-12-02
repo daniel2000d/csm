5ffaced0ae8bd8b3dab1a0be5726e700828489d8
Alex Cornea <alexandru.cornea@student.usv.ro>
2022-12-02 16:09:30 +0200

    Login page design

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from './Components/Footer';
 import Home from './pages/Home';
 import Info from './pages/Info';
 import Matches from './pages/Matches';
+import Login from './pages/Login';
 import {Routes, Route} from 'react-router-dom';
 
 function App() {
@@ -13,14 +14,20 @@ function App() {
           <NavBar />
           
           <Routes>
-            {/* Home Route */}
+            {/* Not Found Route */}
             <Route path="/" element={<Home />} />
+            
+            {/* Home Route */ }
+            <Route path="/home" element={<Home />} />
 
             {/* Info Route */}
             <Route path="/info" element={<Info />} />
 
             {/* Matches Route */}
             <Route path="/matches" element={<Matches />} />
+
+            {/* Login Route */}
+            <Route path="/login" element={<Login />} />
           </Routes>
       </div>
       <Footer />
